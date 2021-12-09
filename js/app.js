b98117f0a457a22be68a2fa59e4b194175bc2a44
Md. Mithon Ali <82331340+mithon42@users.noreply.github.com>
2021-12-09 22:49:01 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function openNav(){document.getElementById("myNav").style.width="100%"}function closeNav(){document.getElementById("myNav").style.width="0%"}$(document).ready(function(){$("button").click(function(){$("index.html").load(service.html)})});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert('আমাদের ওয়েবসাইটে আসার জন্য আপনাকে ধন্যবাদ..')
+
